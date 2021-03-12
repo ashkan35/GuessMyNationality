a01184ada592f4efad8e70e5f9bf1ca26eee9457
Ashkan Semsar <ashkan35@yahoo.com>
2021-03-12 12:54:16 +0330

    Add Ui

---
+++ GuessMyNationality/Areas/Admin/Controllers/GamesController.cs
@@ -0,0 +1,26 @@
+﻿using GuessMyNationality.Data.Repository;
+using GuessMyNationality.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessMyNationality.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GamesController : Controller
+    {
+        private readonly IRepository<Game> _gamerepository;
+
+        public GamesController(IRepository<Game> Gamerepository)
+        {
+           _gamerepository = Gamerepository;
+        }
+        public Task<ViewResult> Index()
+        {
+            var games = _gamerepository.TableNoTracking;
+            return Task.Run(() => { return View(games); });
+        }
+    }
+}
