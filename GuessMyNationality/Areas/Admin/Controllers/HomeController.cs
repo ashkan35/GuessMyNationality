ed98daf86c742531cfdd1745b5d8ac3dfa90533f
Ashkan Semsar <ashkan35@yahoo.com>
2021-03-14 20:15:20 +0330

    Design Dashbord and  controller methods

--- GuessMyNationality/Areas/Admin/Controllers/HomeController.cs
+++ GuessMyNationality/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GuessMyNationality.Data.Repository;
+using GuessMyNationality.Domain.Models;
+using GuessMyNationality.MVC.Areas.Admin.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,23 @@ namespace GuessMyNationality.MVC.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-        
+        private readonly IRepository<Game> _gameRepository;
+
+        public HomeController(IRepository<Game> GameRepository)
+        {
+            _gameRepository = GameRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var games = _gameRepository.TableNoTracking;
+            var query = games.GroupBy(x => x.IpAddress).Select(g => new { IpAddress = g.Key, Count = g.Count() });
+            DashboardViewModel model = new DashboardViewModel
+            {
+                TopScore = games.OrderByDescending(x => x.Score).First().Score,
+                TotalGames = games.Count(),
+                TotalUsers = query.Count()
+            };
+            return View(model);
         }
     }
 }
---
+++ GuessMyNationality/Areas/Admin/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessMyNationality.MVC.Areas.Admin.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalGames { get; set; }
+        public int TopScore { get; set; }
+        public int TotalUsers { get; set; }
+    }
+}
